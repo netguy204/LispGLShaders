b264fde3bd65576dcfdb12a3155aeee1c00704b8
Brian Taylor <el.wubo@gmail.com>
2010-08-25 22:24:23 -0400

    much better cell shader

--- test.f.glsl
+++ test.f.glsl
@@ -1,25 +1,19 @@
 varying vec3 normal;
 varying vec3 vertex_to_light_vector;
 
+float cel(float d) {
+    return smoothstep(0.35, 0.37, d) * 0.4 + smoothstep(0.70, 0.72, d) * 0.6;
+}
+
+float warp_diffuse(float d) {
+    return cel(d * 0.5 + 0.5);
+}
+
 void main()
 {
     const vec4 AmbientColor = vec4(0.0, 0.0, 0.1, 1.0);
-    const vec4 DiffuseColor = vec4(0.0, 0.8, 0.0, 1.0);
-
-    float DiffuseTerm = clamp(
-       dot(normalize(normal),
-           normalize(vertex_to_light_vector)), 0.0, 1.0);
-    
-    vec4 color;
-
-    if(DiffuseTerm > 0.90)
-        color = vec4(1.0, 0.5, 0.5, 1.0);
-    else if (DiffuseTerm > 0.5)
-    	color = vec4(0.6, 0.3, 0.3, 1.0);
-    else if (DiffuseTerm > 0.35)
-        color = vec4(0.4, 0.2, 0.2, 1.0);
-    else
-        color = vec4(0.2, 0.1, 0.1, 1.0);
-
-    gl_FragColor = color;
+    const vec4 DiffuseColor = vec4(0.3, 0.7, 0.3, 1.0);
+    float dp = max(dot(normalize(normal), normalize(vertex_to_light_vector)), 0.0);
+    float DiffuseTerm = warp_diffuse(dp);
+    gl_FragColor = DiffuseColor * DiffuseTerm;    
 }
